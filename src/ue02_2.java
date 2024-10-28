6e5f25aeded3fc8f4b27d47ed1e9c2ed3b13c1ad
Ylvi F <s2410238016@fhooe.at>
2024-10-28 19:52:07 +0100

    version from 28.10.2024, leap year needs to be fixed

--- src/ue02_2.java
+++ src/ue02_2.java
@@ -1,7 +1,40 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_2 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Out.println("Please enter year");
+        int year = In.readInt();
+
+        Out.println("Please enter month (1-12)");
+        int month = In.readInt();
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                Out.print("days: 31");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                Out.print("days: 30");
+                break;
+            case 2: 
+                if (leapYear(year)) {
+                    Out.print("days: 29");
+                } else {
+                    Out.print("days:28");
+                }
+                break;
+            default:
+                break;
+        }
+
     }
 }
