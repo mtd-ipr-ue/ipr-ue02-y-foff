c7d7ef558247eca3b513e1cde27848c8b2238cbe
Ylvi F <s2410238016@fhooe.at>
2024-10-28 15:16:59 +0100

    finished ue2_1

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,28 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+
+        Out.println("Please enter a number");
+        int number1 = In.readInt();
+
+        Out.println("Please enter a number");
+        int number2 = In.readInt();
+
+        Out.println("Please enter a number");
+        int number3 = In.readInt();
+
+        if (number1 >= number2 && number1 >= number3) {
+            Out.print("max:");
+            Out.print(number1);
+        } else if (number2 >= number1 && number2 >= number3) {
+                Out.print("max:");
+                Out.print(number2);
+            }
+         else {
+            Out.print("max:");
+            Out.print(number3);
+        }
     }
 }
